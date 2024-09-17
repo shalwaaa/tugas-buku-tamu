768238ade279ec69ae55a3aec82aae13f3f9eac4
“shalwa <shalwahafidzin@gmail.com>
2024-09-17 08:59:23 +0600

    templating app buku tamu dengan sb admin 2

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include_once('templates/header.php');
+?>
+
+<!-- Begin page content -->
+<div class="container-fluid">
+    <!-- page heading -->
+    <h1 class="h3 mb-4 text-gray-800">Dasboard Admin</h1>
+</div>
+
+<!-- container fluid -->
+
+<?php
+include_once('templates/footer.php')
+?>
