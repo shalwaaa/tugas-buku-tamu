5bd6eaf8b7d7ef907304008cdaf1c7c835cfd77e
“shalwa <shalwahafidzin@gmail.com>
2024-09-30 09:41:15 +0600

    menambah halaman hapus tamu dan fitur hapus data tamu

--- function.php
+++ function.php
@@ -93,5 +93,24 @@ function tambah_user($data)
                 
 }
 
+//function ubah data tamu
+function ubah_user($data)
+{
+    global $koneksi;
+
+    $kode               =htmlspecialchars($data["id_user"]);
+    $username           =htmlspecialchars($data["username"]);
+    $user_role          =htmlspecialchars($data["user_role"]);
+
+    $query =    "UPDATE users SET
+                username        = '$username',
+                user_role       = '$user_role'
+                WHERE id_user   = '$kode'";
+
+    mysqli_query($koneksi, $query);
+
+    return mysqli_affected_rows($koneksi);
+}
+
 
 ?>
